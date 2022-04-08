430f54a13b1d350a99275299ddabc98946703141
Sean Hong(홍성민) <seanhong2000@gmail.com>
2022-04-08 14:57:37 +0900

    Initial Commit

---
+++ lyricsbar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lyricsbar
+//
+//  Created by Sean Hong on 2022/04/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lyricsbar/lyricsbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  lyricsbarApp.swift
+//  lyricsbar
+//
+//  Created by Sean Hong on 2022/04/08.
+//
+
+import SwiftUI
+
+@main
+struct lyricsbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
