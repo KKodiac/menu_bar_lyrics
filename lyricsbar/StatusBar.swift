308432f29eea23bb8cd3dbab442b0331fad8ea40
Sean Hong(홍성민) <seanhong2000@gmail.com>
2022-04-13 01:33:21 +0900

    Add popover that opens app on Mac OS menu bar

---
+++ lyricsbar/StatusBar.swift
@@ -0,0 +1,47 @@
+//
+//  Music.swift
+//  lyricsbar
+//
+//  Created by Sean Hong on 2022/04/08.
+//
+
+import AppKit
+import Foundation
+
+class StatusBarController {
+    private var statusBar: NSStatusBar
+    private var statusItem: NSStatusItem
+    private var popover: NSPopover
+    
+    init(_ popover: NSPopover) {
+        self.popover = popover
+        statusBar = NSStatusBar.init()
+        statusItem = statusBar.statusItem(withLength: 28.0)
+        
+        if let statusBarButton = statusItem.button {
+            statusBarButton.image = NSImage(imageLiteralResourceName: "Lyrics")
+            statusBarButton.image?.size = NSSize(width: 18.0, height: 18.0)
+            statusBarButton.image?.isTemplate = true
+            statusBarButton.action = #selector(togglePopover(sender:))
+            statusBarButton.target = self
+        }
+    }
+    
+    @objc func togglePopover(sender: AnyObject) {
+        if(popover.isShown) {
+            hidePopover(sender)
+        } else {
+            showPopover(sender)
+        }
+    }
+    
+    func showPopover(_ sender: AnyObject) {
+        if let statusBarButton = statusItem.button {
+            popover.show(relativeTo: statusBarButton.bounds, of: statusBarButton, preferredEdge: NSRectEdge.maxY)
+        }
+    }
+    
+    func hidePopover(_ sender: AnyObject) {
+        popover.performClose(sender)
+    }
+}
--- lyricsbar/lyricsbarApp.swift
+++ lyricsbar/lyricsbarApp.swift
@@ -9,9 +9,32 @@ import SwiftUI
 
 @main
 struct lyricsbarApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+        var popover = NSPopover.init()
+        var statusBar: StatusBarController?
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Popover 가 아닌 Window, 즉 맨 처음 생성되는 창을 닫아줌
+        if let window = NSApplication.shared.windows.first {
+            window.close()
+        }
+        let contentView = ContentView()
+        popover.contentSize = NSSize(width: 400, height: 200)
+        popover.contentViewController = NSHostingController(rootView: contentView)
+        popover.setValue(true, forKeyPath: "shouldHideAnchor")
+        statusBar = StatusBarController.init(popover)
+    }
+    
+    func applicationWillTerminate(_ notification: Notification) {
+        // TODO: Termination Code
     }
 }
